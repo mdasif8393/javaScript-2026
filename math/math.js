4f947a359092d07a3b52262d00d4761a3db8c159
mdasif8393 <md.asif.8393@gmail.com>
2026-07-03 17:09:54 +0600

    math

--- math/math.js
+++ math/math.js
@@ -1,1 +1,6 @@
-console.log(Math.floor(Math.random() * 10) + 1);
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const randomNumber = getRndInteger(1000, 9999);
+console.log(randomNumber);
