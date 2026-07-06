cdee65ec31c5a224f8055b747ef26e142b69d50c
mdasif8393 <md.asif.8393@gmail.com>
2026-07-06 21:12:57 +0600

    sort roll numbers

--- problemSolving/problemSolving1.js
+++ problemSolving/problemSolving1.js
@@ -1,6 +1,16 @@
+// *random number between 1 to 6
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const result = getRndInteger(1, 6);
 console.log(result);
+
+//* Alphabetically sort names
+const students = ["Sumit", "Saad", "Daiyan"]
+console.log(students.sort());
+
+//* roll number sort
+const roll_numbers = [3, 5, 1, 6, 2, 30];
+const sortRoll = roll_numbers.sort(function (a, b) { return a - b });
+console.log(sortRoll);
