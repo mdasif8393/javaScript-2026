573e9f40fd0273eb52db0b8f5e5e5fcbcdf5ebe4
mdasif8393 <md.asif.8393@gmail.com>
2026-07-07 11:20:43 +0600

    fromEntries

--- objectAdvance/objectAdvance.js
+++ objectAdvance/objectAdvance.js
@@ -1,5 +1,12 @@
-// "use strict";
-function myFunction() {
-    alert(this); // will alert "undefined"
-}
-myFunction();
+const person1 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+};
+
+// Create Source Object
+const person2 = { firstName: "Anne", lastName: "Smith" };
+
+const myObj = Object.assign(person1, person2);
+console.log(myObj);
--- problemSolving/problemSolving1.js
+++ problemSolving/problemSolving1.js
@@ -14,3 +14,29 @@ console.log(students.sort());
 const roll_numbers = [3, 5, 1, 6, 2, 30];
 const sortRoll = roll_numbers.sort(function (a, b) { return a - b });
 console.log(sortRoll);
+
+//* Leap Year
+function isLeapYear(year) {
+    if (((year % 400 === 0) || (year % 4 === 0)) && year % 100 !== 0) {
+        console.log(`${year} is a leapyear`);
+    }
+    else {
+        console.log(`${year} is not a leapyear`);
+    }
+}
+
+isLeapYear(2028)
+
+//* count vowel
+// const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+// function countVowel(sentence) {
+//     let count = 0;
+//     const letters = Array.from(sentence);
+//     letters.forEach function(value, index, array) {
+
+//     }
+
+// }
+
+// console.log(countVowel("I Love Bangladesh"));
