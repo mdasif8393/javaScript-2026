2e066130b1660a00848ee11c0a717733ba343eff
mdasif8393 <md.asif.8393@gmail.com>
2026-07-08 21:25:01 +0600

    count vowels

--- functionAdvanced/functionAdvanced.js
+++ functionAdvanced/functionAdvanced.js
@@ -1,14 +1,5 @@
-function a(x) {
-    x.one = 7;
-
-    return x.one * x.two;
+function sleep() {
+    console.log("I am sleeping");
 }
 
-let m = {
-    one: 4,
-    two: 5
-};
-
-console.log(a(m));
-
-console.log(m);
+sleep(); 
--- problemSolving/problemSolving1.js
+++ problemSolving/problemSolving1.js
@@ -1,42 +1,19 @@
-// *random number between 1 to 6
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
-const result = getRndInteger(1, 6);
-console.log(result);
-
-//* Alphabetically sort names
-const students = ["Sumit", "Saad", "Daiyan"]
-console.log(students.sort());
-
-//* roll number sort
-const roll_numbers = [3, 5, 1, 6, 2, 30];
-const sortRoll = roll_numbers.sort(function (a, b) { return a - b });
-console.log(sortRoll);
-
-//* Leap Year
-function isLeapYear(year) {
-    if (((year % 400 === 0) || (year % 4 === 0)) && year % 100 !== 0) {
-        console.log(`${year} is a leapyear`);
-    }
-    else {
-        console.log(`${year} is not a leapyear`);
-    }
-}
-
-isLeapYear(2028)
 
 //* count vowel
-// const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
-// function countVowel(sentence) {
-//     let count = 0;
-//     const letters = Array.from(sentence);
-//     letters.forEach function(value, index, array) {
+function countVowel(sentence) {
+    let count = 0;
+    const letters = Array.from(sentence);
+    letters.forEach(function (value, index, array) {
+        if (vowels.includes(value)) {
+            count++
+        }
+    })
 
-//     }
+    return count;
+}
 
-// }
-
-// console.log(countVowel("I Love Bangladesh"));
+const totalVowels = (countVowel("I Love Bangladesh"));
+console.log(totalVowels);
