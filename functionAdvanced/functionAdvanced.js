da0beb183e7cf83ed1142d0db9428cb083089e6d
mdasif8393 <md.asif.8393@gmail.com>
2026-07-08 13:06:07 +0600

    function expression

---
+++ functionAdvanced/functionAdvanced.js
@@ -0,0 +1,13 @@
+// const x = function (a, b) {
+//     return a + b;
+// }
+
+// const res = x(2, 3);
+// console.log(res);
+
+function myFunction(a, b) {
+    return a + b;
+}
+
+const res = myFunction(2, 3);
+console.log(res);
