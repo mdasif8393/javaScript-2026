58842b40bc59d2bbf907ac18de9ab8c67182e04a
mdasif8393 <md.asif.8393@gmail.com>
2026-07-15 20:48:51 +0600

    https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/M

--- dom/changingCss.js
+++ dom/changingCss.js
@@ -1,8 +1,8 @@
 function show() {
     console.log("Click");
-    document.getElementById("p1").style.visibility = 'hidden';
+    ;
 }
 
 function show() {
-    document.getElementById("p1").style.visibility = 'visible';
+    ;
 }
--- dom/formValidation.js
+++ dom/formValidation.js
@@ -1,11 +1,1 @@
-function validateForm() {
-    const form = document.forms['myForm'];
-    const value = form['fname'].value;
-
-    if (value === "") {
-        alert("You must type name");
-        return false;
-    }
-
-
-}
+console.log(document.URL);
