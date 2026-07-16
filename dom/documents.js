6396a2d1f2485eb8735fbccc06ef5c757f83797f
mdasif8393 <md.asif.8393@gmail.com>
2026-07-16 21:20:45 +0600

    dom navigation

--- dom/documents.js
+++ dom/documents.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("html loaded");
-});
+const p = document.getElementById("demo");
+
+const way1 = p.innerHTML;
+
+const way2 = p.firstChild.nodeValue;
+
+const way3 = p.childNodes[0].nodeName;
+
+console.log(way3);
