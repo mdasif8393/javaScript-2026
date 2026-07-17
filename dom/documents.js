e53ba1c41002a1126abfb9b906df6ca75f97215f
mdasif8393 <md.asif.8393@gmail.com>
2026-07-17 14:08:49 +0600

    https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/M

--- dom/documents.js
+++ dom/documents.js
@@ -1,9 +1,1 @@
-const p = document.getElementById("demo");
-
-const way1 = p.innerHTML;
-
-const way2 = p.firstChild.nodeValue;
-
-const way3 = p.childNodes[0].nodeName;
-
-console.log(way3);
+console.dir(document.getElementsByTagName("p"));
